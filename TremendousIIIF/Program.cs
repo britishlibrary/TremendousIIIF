c8398ca9f45109e8e7412ab73e2c1d6d169750ac
Thomas Hatton <thomas.hatton@bl.uk>
2023-06-27 16:27:55 +0100

    SAPI-711 Enable Swagger UI For Testing Deployment
    - Include the Swagger UI Temporarily for a production release.

--- TremendousIIIF/Program.cs
+++ TremendousIIIF/Program.cs
@@ -74,12 +74,12 @@ builder.Services.AddLazyCache();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("./v1/swagger.json", "TremendousIIIF");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TremendousIIIF");
     });
     app.UseDeveloperExceptionPage();
 }
